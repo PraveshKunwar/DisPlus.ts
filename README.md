8e6e301352434667e639b466c55a85b385d4802b
Pravesh Kunwar <praveshk04@outlook.com>
2021-08-25 23:16:53 -0400

    feat: init commits

---
+++ README.md
@@ -0,0 +1,4 @@
+# Displus.ts
+
+Welcome to Displus.ts! This is a package with utility methods for the popular library,
+discord.js.
---
+++ src/util/BaseUtil.ts
@@ -0,0 +1,28 @@
+import {
+  Base,
+  Client,
+  Guild,
+  GuildMember,
+  Message,
+  Snowflake,
+} from "discord.js";
+
+export default class BaseUtil {
+  protected client: Client;
+  public constructor(client: Client) {
+    this.client = client;
+  }
+  public isSnowflake(verify: string | Snowflake): boolean {
+    if (typeof verify !== "string") {
+      return false;
+    } else return true;
+  }
+  public isGuild(guild: Guild): this is Guild {
+    if (guild instanceof Guild) {
+      return true;
+    } else return false;
+  }
+  public async isMember(id: Snowflake) {
+    const getMember = await this.client.users.fetch(id);
+  }
+}
